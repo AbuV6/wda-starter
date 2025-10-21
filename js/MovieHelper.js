55f87b7da8d95267face9e1287eee5b76565ca9f
abubakr-vali_msmg <abubakr.vali@moneysupermarket.com>
2025-10-21 10:14:00 +0100

    feat: added back buttons, ratings and cast members

--- js/MovieHelper.js
+++ js/MovieHelper.js
@@ -1,47 +1,71 @@
 export default class MovieHelper {
-    constructor() {
-        this.api_root = "https://api.themoviedb.org/3"
-        this.api_key = "6d86a2175c01f3eb4825fb5fc418fc33"
-    }
+  constructor() {
+    this.api_root = "https://api.themoviedb.org/3";
+    this.api_key = "6d86a2175c01f3eb4825fb5fc418fc33";
+  }
 
-    async searchMovies(query, year = "") {
-        const url = new URL(`${this.api_root}/search/movie`)
-        url.searchParams.append("api_key", this.api_key)
-        url.searchParams.append("query", query)
-        if (year) url.searchParams.append("year", year)
+  async searchMovies(query, year = "") {
+    const url = new URL(`${this.api_root}/search/movie`);
+    url.searchParams.append("api_key", this.api_key);
+    url.searchParams.append("query", query);
+    if (year) url.searchParams.append("year", year);
 
-        const res = await fetch(url)
-        const data = await res.json()
-        return data.results || []
-    }
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results || [];
+  }
 
-    async getMovieDetails(movie_id) {
-        const url = `${this.api_root}/movie/${movie_id}?api_key=${this.api_key}`
-        const res = await fetch(url)
-        return await res.json()
-    }
+  async getMovieDetails(movie_id) {
+    const url = `${this.api_root}/movie/${movie_id}?api_key=${this.api_key}`;
+    const res = await fetch(url);
+    return await res.json();
+  }
 
-    async discoverMovies(year = "", genreId = "") {
-        const url = new URL(`${this.api_root}/discover/movie`)
-        url.searchParams.append("api_key", this.api_key)
-        if (year) url.searchParams.append("primary_release_year", year)
-        if (genreId) url.searchParams.append("with_genres", genreId)
+  async getMovieCredits(movie_id) {
+    const url = `${this.api_root}/movie/${movie_id}/credits?api_key=${this.api_key}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.cast || [];
+  }
 
-    const res = await fetch(url)
-    const data = await res.json()
-    return data.results || []
-}
-    async getGenres() {
-        const url = `${this.api_root}/genre/movie/list?api_key=${this.api_key}`
-        const res = await fetch(url)
-        const data = await res.json()
-        return data.genres || []
-    }
+  async discoverMovies(year = "", genreId = "") {
+    const url = new URL(`${this.api_root}/discover/movie`);
+    url.searchParams.append("api_key", this.api_key);
+    if (year) url.searchParams.append("primary_release_year", year);
+    if (genreId) url.searchParams.append("with_genres", genreId);
 
-    async discoverByGenre(genreId) {
-        const url = `${this.api_root}/discover/movie?api_key=${this.api_key}&with_genres=${genreId}`
-        const res = await fetch(url)
-        const data = await res.json()
-        return data.results || []
-    }
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results || [];
+  }
+
+  async getGenres() {
+    const url = `${this.api_root}/genre/movie/list?api_key=${this.api_key}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.genres || [];
+  }
+
+  async discoverByGenre(genreId) {
+    const url = `${this.api_root}/discover/movie?api_key=${this.api_key}&with_genres=${genreId}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    return data.results || [];
+  }
+
+  async rateMovie(movie_id, rating) {
+    const guestSession = await fetch(
+      `${this.api_root}/authentication/guest_session/new?api_key=${this.api_key}`
+    );
+    const { guest_session_id } = await guestSession.json();
+
+    const url = `${this.api_root}/movie/${movie_id}/rating?api_key=${this.api_key}&guest_session_id=${guest_session_id}`;
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json;charset=utf-8" },
+      body: JSON.stringify({ value: rating })
+    });
+
+    return await res.json();
+  }
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,119 @@
-let MovieHelper
+let MovieHelper;
 
 async function loadMovieHelper() {
-    if (!MovieHelper) {
-        const module = await import('./MovieHelper.js')
-        MovieHelper = new module.default()
-    }
-    return MovieHelper
+  if (!MovieHelper) {
+    const module = await import('./MovieHelper.js');
+    MovieHelper = new module.default();
+  }
+  return MovieHelper;
 }
 
 function getUrlParam(param) {
-    const urlParams = new URLSearchParams(window.location.search)
-    return urlParams.get(param)
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
 }
 
 let movieListComponent = {
-    movies: [],
-    search: "",
-    selectedYear: "",
-    years: [],
-    genres: [],
-    selectedGenre: "",
-    error: null,
+  movies: [],
+  search: "",
+  selectedYear: "",
+  years: [],
+  genres: [],
+  selectedGenre: "",
+  error: null,
 
-    async init() {
-        const MH = await loadMovieHelper()
-        this.movies = await MH.discoverMovies()
+  async init() {
+    const MH = await loadMovieHelper();
+    this.movies = await MH.discoverMovies();
 
-        const currentYear = new Date().getFullYear()
-        this.years = []
-        for (let y = currentYear; y >= 1980; y--) {
-            this.years.push(y)
-        }
-
-        this.genres = await MH.getGenres()
-    },
-    async applyFilters() {
-        const MH = await loadMovieHelper()
-        const query = this.search.trim()
-        const year = this.selectedYear
-        const genre = this.selectedGenre
-        if (query) {
-            this.movies = await MH.searchMovies(query, year)
-        } 
-        else {
-            this.movies = await MH.discoverMovies(year, genre)
-        }
-    },
-
-    async doSearchByName() {
-        await this.applyFilters()
-    },
-
-    async doSearchByYear() {
-        await this.applyFilters()
-    },
-
-    async doSearchByGenre() {
-        await this.applyFilters()
-    },
-
-    addToWatchlist(movie) {
-        let list = JSON.parse(localStorage.getItem("watchlist") || "[]")
-        if (!list.find(m => m.id === movie.id)) {
-            list.push(movie)
-            localStorage.setItem("watchlist", JSON.stringify(list))
-            alert(`${movie.title} added to watchlist!`)
-        }
+    const currentYear = new Date().getFullYear();
+    for (let y = currentYear; y >= 1980; y--) {
+      this.years.push(y);
     }
-}
+
+    this.genres = await MH.getGenres();
+  },
+
+  async doSearchByName() {
+    const MH = await loadMovieHelper();
+    if (!this.search.trim()) {
+      this.movies = await MH.discoverMovies();
+      return;
+    }
+    this.movies = await MH.searchMovies(this.search);
+  },
+
+  async doSearchByYear() {
+    const MH = await loadMovieHelper();
+    if (!this.selectedYear) {
+      this.movies = await MH.discoverMovies();
+      return;
+    }
+    this.movies = await MH.discoverMovies(this.selectedYear);
+  },
+
+  async doSearchByGenre() {
+    const MH = await loadMovieHelper();
+    if (!this.selectedGenre) {
+      this.movies = await MH.discoverMovies();
+      return;
+    }
+    this.movies = await MH.discoverByGenre(this.selectedGenre);
+  },
+
+  addToWatchlist(movie) {
+    let list = JSON.parse(localStorage.getItem("watchlist") || "[]");
+    if (!list.find(m => m.id === movie.id)) {
+      list.push(movie);
+      localStorage.setItem("watchlist", JSON.stringify(list));
+      alert(`${movie.title} added to watchlist!`);
+    }
+  }
+};
 
 let movieComponent = {
-    movie: null,
-    async init() {
-        const id = getUrlParam("movie_id")
-        if (id) {
-            const MH = await loadMovieHelper()
-            this.movie = await MH.getMovieDetails(id)
-        }
-    },
-    addToWatchlist(movie) {
-        let list = JSON.parse(localStorage.getItem("watchlist") || "[]")
-        if (!list.find(m => m.id === movie.id)) {
-            list.push(movie)
-            localStorage.setItem("watchlist", JSON.stringify(list))
-            alert(`${movie.title} added to watchlist!`)
-        }
+  movie: null,
+  userRating: 0,
+
+  async init() {
+    const id = getUrlParam("movie_id");
+    if (id) {
+      const MH = await loadMovieHelper();
+      this.movie = await MH.getMovieDetails(id);
+      this.cast = await MH.getMovieCredits(id);
     }
-}
+  },
+
+  async rateMovie(value) {
+    this.userRating = value;
+    const MH = await loadMovieHelper();
+    const response = await MH.rateMovie(this.movie.id, value);
+    if (response.success) {
+      alert(`You rated ${this.movie.title} ${value}/10 ⭐`);
+    } else {
+      alert("Failed to submit rating, try again later.");
+    }
+  },
+
+  addToWatchlist(movie) {
+    let list = JSON.parse(localStorage.getItem("watchlist") || "[]");
+    if (!list.find(m => m.id === movie.id)) {
+      list.push(movie);
+      localStorage.setItem("watchlist", JSON.stringify(list));
+      alert(`${movie.title} added to watchlist!`);
+    }
+  }
+};
 
 let watchlistComponent = {
-    watchlist: [],
-    init() {
-        this.watchlist = JSON.parse(localStorage.getItem("watchlist") || "[]")
-    },
-    remove(id) {
-        this.watchlist = this.watchlist.filter(m => m.id !== id)
-        localStorage.setItem("watchlist", JSON.stringify(this.watchlist))
-    }
-}
+  watchlist: [],
+
+  init() {
+    this.watchlist = JSON.parse(localStorage.getItem("watchlist") || "[]");
+  },
+
+  remove(id) {
+    this.watchlist = this.watchlist.filter(m => m.id !== id);
+    localStorage.setItem("watchlist", JSON.stringify(this.watchlist));
+  }
+};
